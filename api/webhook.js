c8ad7d66898c062c14c8439fbeb95df6cf43800a
djamal195 <djamaldinemontana@gmail.com>
2025-02-25 13:48:45 +0300

    Update webhook.js

--- api/webhook.js
+++ api/webhook.js
@@ -11,7 +11,7 @@ app.use(bodyParser.json())
 // Logging middleware
 app.use((req, res, next) => {
   console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`)
-  console.log("Headers:", req.headers)
+  console.log("Query:", req.query)
   console.log("Body:", req.body)
   next()
 })
